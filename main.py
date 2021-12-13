91a79509564f0fa7bb1b86e13b0cf35522154cfe
Vym38224 <jardaavymetal@gmail.com>
2021-12-13 09:33:05 +0100

    zmena ve skole

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-from tkinter import Scale, HORIZONTAL, Canvas
+from tkinter import Scale, HORIZONTAL, Canvas, LEFT, Frame, Entry, S, StringVar
 
 # from tkinter import ttk
 
@@ -18,20 +18,36 @@ class Application(tk.Tk):
 
         self.bind("<Escape>", self.quit) #propojí klávesovou zkratku s nějakou funkcí (Esc spustí funkci quit())
 
-        self.lbl = tk.Label(self, text="R") #nápis co se tam zobrazuje (label)
-        self.lbl.pack() #umístění napísu
-        self.scaleR = Scale(self, from_ = 0, to = 255, orient = HORIZONTAL, length = 256, command = self.change)
-        self.scaleR.pack()
+        self.frameR = Frame(self)
+        self.frameR.pack()
+        self.frameG = Frame(self)
+        self.frameG.pack()
+        self.frameB = Frame(self)
+        self.frameB.pack()
 
-        self.lbl = tk.Label(self, text="G")
-        self.lbl.pack()
-        self.scaleR = Scale(self, from_ = 0, to = 255, orient = HORIZONTAL, length = 256)
-        self.scaleR.pack()
+        self.lbl = tk.Label(self.frameR, text="R:") #nápis co se tam zobrazuje (label)
+        self.lbl.pack(side = LEFT, anchor=S) #umístění napísu
+        self.scaleR = Scale(self.frameR, from_ = 0, to = 255, orient = HORIZONTAL, length = 256, command = self.change)
 
-        self.lbl = tk.Label(self, text="B")
-        self.lbl.pack()
-        self.scaleR = Scale(self, from_ = 0, to = 255, orient = HORIZONTAL, length = 256)
-        self.scaleR.pack()
+        self.scaleR.pack(side = LEFT, anchor=S)
+        self.entryR = Entry(self.frameR, width = 4)
+        self.entryR.pack(side = LEFT, anchor=S)
+
+        self.lbl = tk.Label(self.frameG, text="G:")
+        self.lbl.pack(side = LEFT, anchor=S)
+        self.scaleG = Scale(self.frameG, from_ = 0, to = 255, orient = HORIZONTAL, length = 256, command = self.change)
+
+        self.scaleG.pack(side = LEFT, anchor=S)
+        self.entryG = Entry(self.frameG, width = 4)
+        self.entryG.pack(side = LEFT, anchor=S)
+
+        self.lbl = tk.Label(self.frameB, text="B:")
+        self.lbl.pack(side = LEFT, anchor=S)
+        self.scaleB = Scale(self.frameB, from_ = 0, to = 255, orient = HORIZONTAL, length = 256, command = self.change)
+      
+        self.scaleB.pack(side = LEFT, anchor=S)
+        self.entryB = Entry(self.frameB, width = 4)
+        self.entryB.pack(side = LEFT, anchor=S)
 
         self.canvasMain = Canvas(self, width = 256, height = 100, background = "#000000")
         self.canvasMain.pack()
@@ -46,9 +62,10 @@ class Application(tk.Tk):
     
     def change(self, event):
         r = self.scaleR.get()
-        g = self.scaleR.get()
-        b = self.scaleR.get()
-        self.canvasMain.config(Background = f"{r:02x}{g:02x}{b:02x}")
+        g = self.scaleG.get()
+        b = self.scaleB.get()
+        self.canvasMain.config(background = f"#{r:02x}{g:02x}{b:02x}")
+        print(f"#{r:02x}{g:02x}{b:02x}")
 
     def quit(self, event=None):
         super().quit()
