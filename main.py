fb2a680ff1db4b6b4e7626579f430b59c47187a5
Vym38224 <jardaavymetal@gmail.com>
2022-01-18 20:05:44 +0100

    úpravadoma

--- main.py
+++ main.py
@@ -90,10 +90,11 @@ class Application(tk.Tk):
         self.varG.trace("w", self.change)
         self.varB.trace("w", self.change)
         self.load()
-
-
+        self.canvasMain2scales()
     
     def change(self, var, index, mode):
+        if(self.ignoreChange):
+            return
         r = self.scaleR.get()
         g = self.scaleG.get()
         b = self.scaleB.get()
@@ -111,6 +112,7 @@ class Application(tk.Tk):
             self.color = event.widget.cget("background")
         elif self.cget("cursor")=="pencil" and event.widget != self.canvasMain:
             self.config(cursor = "")
+            event.widget.config()
             event.widget.config(background = self.color)
         else:
             self.color = event.widget.cget("background")   
@@ -118,7 +120,7 @@ class Application(tk.Tk):
             #self.varR.scaleR.get()
             #self.varG.scaleG.get()
             #self.varB.scaleB.get()
-            None
+            self.canvasMain2scales()
 
 
     #uložení do příště
@@ -129,9 +131,14 @@ class Application(tk.Tk):
         r = int(color[1:3], 16)
         g = int(color[3:5], 16)
         b = int(color[5:7], 16)
+        self.ignoreChange = True
         self.varR.set(r)
         self.varG.set(g)
         self.varB.set(b)
+        self.scaleR.set(r)
+        self.scaleG.set(g)
+        self.scaleB.set(b)
+        self.ignoreChange = False
     
     
     def load(self):
